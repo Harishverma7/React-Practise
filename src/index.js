d1328d3bedc7d184a796b7aeeeb85fec6a02a2ab
Harish <harish.verma@iwebcode.dev>
2023-01-25 14:12:13 +0530

    Changes

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import Card from "./Card";
-import Aray from "./Aray";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 
@@ -11,19 +9,6 @@ root.render(
   <>
     <React.StrictMode>
       <App />
-      {Aray.map((val, index) => {
-        console.log(index);
-        return (
-
-          <Card
-            key={val.id}
-            imgsrc={val.imgsrc}
-            title={val.title}
-            button={val.button}
-          />
-
-        )
-      })}
     </React.StrictMode>
   </>
 );
